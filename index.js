b28cd8c40bce9d9cc60dfcc0724f86d42449f0f1
NAUTH05 <nguyenthuan.dev05@gmail.com>
2025-11-07 13:55:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,4 +36,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
+    console.log(`Access at http://localhost:${port}/`);
 });
